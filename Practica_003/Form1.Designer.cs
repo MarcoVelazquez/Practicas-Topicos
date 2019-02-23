ce8005bd58ec3aba0b3c7d2a78c809c702f96071
Marco Velazquez <antoniovz377@gmail.com>
2019-02-22 19:07:36 -0800

    Reacomodo 22/02/2019

---
+++ Practica_003/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Practica_003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Circulo = new System.Windows.Forms.Button();
+            this.Triangulo = new System.Windows.Forms.Button();
+            this.Cuadrado = new System.Windows.Forms.Button();
+            this.Rectangulo = new System.Windows.Forms.Button();
+            this.Dodecagono = new System.Windows.Forms.Button();
+            this.Pentagono = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(493, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Circulo
+            // 
+            this.Circulo.Location = new System.Drawing.Point(687, 84);
+            this.Circulo.Name = "Circulo";
+            this.Circulo.Size = new System.Drawing.Size(80, 80);
+            this.Circulo.TabIndex = 1;
+            this.Circulo.Text = "button1";
+            this.Circulo.UseVisualStyleBackColor = true;
+            // 
+            // Triangulo
+            // 
+            this.Triangulo.Location = new System.Drawing.Point(604, 84);
+            this.Triangulo.Name = "Triangulo";
+            this.Triangulo.Size = new System.Drawing.Size(80, 80);
+            this.Triangulo.TabIndex = 2;
+            this.Triangulo.Text = "button2";
+            this.Triangulo.UseVisualStyleBackColor = true;
+            // 
+            // Cuadrado
+            // 
+            this.Cuadrado.Location = new System.Drawing.Point(521, 84);
+            this.Cuadrado.Name = "Cuadrado";
+            this.Cuadrado.Size = new System.Drawing.Size(80, 80);
+            this.Cuadrado.TabIndex = 3;
+            this.Cuadrado.Text = "button3";
+            this.Cuadrado.UseVisualStyleBackColor = true;
+            // 
+            // Rectangulo
+            // 
+            this.Rectangulo.Location = new System.Drawing.Point(521, 178);
+            this.Rectangulo.Name = "Rectangulo";
+            this.Rectangulo.Size = new System.Drawing.Size(80, 80);
+            this.Rectangulo.TabIndex = 4;
+            this.Rectangulo.Text = "button4";
+            this.Rectangulo.UseVisualStyleBackColor = true;
+            // 
+            // Dodecagono
+            // 
+            this.Dodecagono.Location = new System.Drawing.Point(604, 178);
+            this.Dodecagono.Name = "Dodecagono";
+            this.Dodecagono.Size = new System.Drawing.Size(80, 80);
+            this.Dodecagono.TabIndex = 5;
+            this.Dodecagono.Text = "button5";
+            this.Dodecagono.UseVisualStyleBackColor = true;
+            // 
+            // Pentagono
+            // 
+            this.Pentagono.Location = new System.Drawing.Point(687, 178);
+            this.Pentagono.Name = "Pentagono";
+            this.Pentagono.Size = new System.Drawing.Size(80, 80);
+            this.Pentagono.TabIndex = 6;
+            this.Pentagono.Text = "button6";
+            this.Pentagono.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Blue;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Location = new System.Drawing.Point(8, 378);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(40, 40);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button8.Location = new System.Drawing.Point(238, 378);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(40, 40);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Location = new System.Drawing.Point(192, 378);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(40, 40);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Location = new System.Drawing.Point(284, 378);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(40, 40);
+            this.button10.TabIndex = 10;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Yellow;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Location = new System.Drawing.Point(330, 378);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(40, 40);
+            this.button11.TabIndex = 11;
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Lime;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button12.Location = new System.Drawing.Point(146, 378);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(40, 40);
+            this.button12.TabIndex = 12;
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Aqua;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button13.ForeColor = System.Drawing.Color.Aqua;
+            this.button13.Location = new System.Drawing.Point(100, 378);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(40, 40);
+            this.button13.TabIndex = 13;
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Fuchsia;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button14.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button14.Location = new System.Drawing.Point(54, 378);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(40, 40);
+            this.button14.TabIndex = 14;
+            this.button14.UseVisualStyleBackColor = false;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(604, 325);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(80, 30);
+            this.Clear.TabIndex = 15;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(806, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.Pentagono);
+            this.Controls.Add(this.Dodecagono);
+            this.Controls.Add(this.Rectangulo);
+            this.Controls.Add(this.Cuadrado);
+            this.Controls.Add(this.Triangulo);
+            this.Controls.Add(this.Circulo);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Circulo;
+        private System.Windows.Forms.Button Triangulo;
+        private System.Windows.Forms.Button Cuadrado;
+        private System.Windows.Forms.Button Rectangulo;
+        private System.Windows.Forms.Button Dodecagono;
+        private System.Windows.Forms.Button Pentagono;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
